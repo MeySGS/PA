292ce473b5d42054d26df3f6145a30ed26d14491
MeySGS <56978172+MeySGS@users.noreply.github.com>
2019-12-03 16:10:27 -0300

    Add files via upload

---
+++ Lista15-6.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Ex6 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+	int [] A = new int [11];
+	int f;
+
+	for(int i=0;i<A.length;i++) {
+		System.out.println(" Digite um número para a " +(i+1) +"° posição ");
+		A[i]=in.nextInt();
+	}
+
+	for(int i=0;i<A.length;) {
+		aux=2*2;
+		f++;
+
+		if(f==A[i]) {
+			System.out.println(" o fatorial da posição " +(i+1) + " = " +A[i]);
+			i++;
+		}
+
+	}
+
+}
+
+}
