3f4c5406c149f4144d174d96db568112be335fd5
MeySGS <56978172+MeySGS@users.noreply.github.com>
2019-12-03 14:59:55 -0300

    Add files via upload

--- Lista15-11.java
+++ Lista15-11.java
@@ -5,25 +5,32 @@ public class Ex11 {
 		Scanner in = new Scanner(System.in);
 		
 		int n,c=0;
-		int VetorA[] = new int [10];
+		int [] VetorA = new int [10];
+		int [] VetorB = new int [10];
+
+		for(int i=0;i<10;i++){
+			
+			B[i] = i*i;
+		}
 		
 		for(int i=0;i<VetorA.length;i++) {
 			System.out.println("Digite um numero pfv");
 			VetorA[i]=in.nextInt();
-		}
+
 		System.out.println("Qual número deseja buscar ?");
-		n=in.nextInt();
-		for(int i=0;i<VetorA.length;i++) {
-			if(n==VetorA[i]) {
-				c=VetorA[i];
-		}
-		}
+		VetorA[i]=in.nextInt();
+			for(int i=0;i<VetorB.length;i++) {
+				if(n==VetorB[i]) {
+					c=VetorA[i];
+				}
+			}
 		if(c==n) {
 			System.out.println(" o número que estava procurando está no Vetor ");
 		}else {
 			System.out.println(" esse número não está no vetor");
 		}
-	
-
+			if(n==VetorA[i])
+			i++;	
+			else if(
 	}
 }
